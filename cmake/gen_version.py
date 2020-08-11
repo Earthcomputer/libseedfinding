9d01b2d6505bf6f2a8719c2853eb16d58681cf55
Neil <h66r10@gmail.com>
2020-08-11 12:23:33 +0200

    [~] Add safe guards

--- cmake/gen_version.py
+++ cmake/gen_version.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding=utf-8 -*- pyversions=2.6+,3.3+
 import json
 import urllib.request
 from urllib.error import HTTPError, URLError
 import fileinput
 import os
+import shutil
+
+
 def download_file(url, filename):
     try:
         print(f'Downloading {filename}...')
@@ -17,17 +21,26 @@ def download_file(url, filename):
         print('URL Error')
         print(e)
 
-download_file("https://launchermeta.mojang.com/mc/game/version_manifest.json", f"version_manifest.json")
-text=[]
+
+
+download_file("https://launchermeta.mojang.com/mc/game/version_manifest.json", "version_manifest.json")
+if not os.path.isfile('version_manifest.json'):
+    exit(-1)
+print("Download of version_manifest.json was a success")
+text = []
 with open("version_manifest.json") as file:
-    versions=json.load(file)["versions"]
+    versions = json.load(file)["versions"]
     versions.reverse()
-    typf=lambda typ:"SNAPSHOT" if typ=="snapshot" else "RELEASE" if typ=="release" else "OLD_ALPHA" if typ=="old_alpha" else "OLD_BETA" if typ=="old_beta" else "NONE"
-    for i,version in enumerate(versions):
-        text.append("const Version MC_"+version.get("id").replace(".","_").replace("-","_").replace(" ","_")+" = {"+str(i)+","+typf(version.get("type"))+',"'+version.get("releaseTime")+'"};')
+    typf = lambda typ: "SNAPSHOT" if typ == "snapshot" else "RELEASE" if typ == "release" else "OLD_ALPHA" \
+        if typ == "old_alpha" else "OLD_BETA" if typ == "old_beta" else "NONE"
+    for i, version in enumerate(versions):
+        text.append("const Version MC_" + version.get("id").replace(".", "_").replace("-", "_").replace(" ", "_") +
+                    " = {" + str(i) + ", " + typf(version.get("type")) + ', "' + version.get("releaseTime") + '"}; ')
 
-
-
+# restore old version.h in case of resync of cmake
+if os.path.isfile('../src/version.h.bak'):
+    print("Detected a backup, restoring it to replace to regenerate version.h")
+    shutil.copyfile('../src/version.h.bak', '../src/version.h')
 with fileinput.FileInput("../src/version.h", inplace=True, backup='.bak') as file:
     for line in file:
-        print(line.replace("/////////////////////////GENERATION////////////////////////////////", (os.linesep+"\t").join(text)), end='')
+        print(line.replace("/////////////////////////GENERATION////////////////////////////////", (os.linesep + "\t").join(text)), end='')
