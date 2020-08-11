dfec196ccfb54eb574932a9714cfac69c238ca63
Neil <h66r10@gmail.com>
2020-08-11 11:57:04 +0200

    [~!] gen version via python

---
+++ cmake/gen_version.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import json
+import urllib.request
+from urllib.error import HTTPError, URLError
+import fileinput
+import os
+def download_file(url, filename):
+    try:
+        print(f'Downloading {filename}...')
+        f = urllib.request.urlopen(url)
+        with open(filename, 'wb+') as local_file:
+            local_file.write(f.read())
+    except HTTPError as e:
+        print('HTTP Error')
+        print(e)
+    except URLError as e:
+        print('URL Error')
+        print(e)
+
+download_file("https://launchermeta.mojang.com/mc/game/version_manifest.json", f"version_manifest.json")
+text=[]
+with open("version_manifest.json") as file:
+    versions=json.load(file)["versions"]
+    versions.reverse()
+    typf=lambda typ:"SNAPSHOT" if typ=="snapshot" else "RELEASE" if typ=="release" else "OLD_ALPHA" if typ=="old_alpha" else "OLD_BETA" if typ=="old_beta" else "NONE"
+    for i,version in enumerate(versions):
+        text.append("\tconst Version","MC_"+version.get("id").replace(".","_").replace("-","_").replace(" ","_"),"= {",i,",",typf(version.get("type")),",",'"'+version.get("releaseTime")+'"',"};")
+
+
+
+with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace("/////////////////////////GENERATION////////////////////////////////", os.linesep.join(text)), end='')
