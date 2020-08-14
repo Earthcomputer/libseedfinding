96d8bbe7088d9e9df0fa0f6518b0bf62e8ffb611
Neil <h66r10@gmail.com>
2020-08-15 00:25:44 +0200

    fix a few renaming mistake

---
+++ example/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) The Minecraft Seed Finding Team
+//
+// MIT License
+
+
+#include <iostream>
+#include <lcg.h>
+
+
+int main() {
+    std::cout << lcg::dfz2seed(47) << std::endl;
+    return 0;
+}
