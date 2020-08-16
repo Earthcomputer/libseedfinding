340fd1901f7314a4d6b717ce3314db9fab488a72
Joe <burtonjae@hotmail.co.uk>
2020-08-16 12:51:15 +0100

    Clean up defines

--- include/util.h
+++ include/util.h
@@ -5,6 +5,12 @@
 #ifndef LIBSEEDFINDING_UTIL_H
 #define LIBSEEDFINDING_UTIL_H
 
+#ifdef _MSVC_LANG
+#define CPP_VER _MSVC_LANG
+#else
+#define CPP_VER __cplusplus
+#endif
+
 #if defined(__CUDACC__)
 #define FORCEINLINE __forceinline__
 #elif defined(__GNUC__)
@@ -25,9 +31,11 @@
 #ifdef __CUDA_ARCH__
 #define DEVICE_FORCEINLINE FORCEINLINE
 #define HOST_FORCEINLINE
+#define DEVICEABLE_CONST __constant__ __device__
 #else
 #define DEVICE_FORCEINLINE
 #define HOST_FORCEINLINE FORCEINLINE
+#define DEVICEABLE_CONST const
 #endif
 
 #endif //LIBSEEDFINDING_UTIL_H
